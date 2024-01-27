700c437673fdbbab90c09cefa0c321dfe44c5148
Manmeet Singh <royalmarine995@gmail.com>
2024-01-27 19:58:54 +0530

    before pull

--- src/components/CompanyRecom.jsx
+++ src/components/CompanyRecom.jsx
@@ -1,36 +1,42 @@
 import React from 'react'
-import { ChevronRight } from '@heroicons/react/24/outline'
+import {CurrencyRupeeIcon, ChevronRightIcon, CubeTransparentIcon } from '@heroicons/react/24/outline'
 const CompanyRecom = (props) => {
   return (
     <>
 
       <div className="w-[25rem] my-2">
-        <div
-          className="relative flex items-center gap-4 py-3 pl-4 pr-8 bg-gray-800 shadow-lg rounded-xl shadow-teal-900/5">
-          <span
-            className="relative z-[2] w-max flex-shrink-0 overflow-hidden rounded-full bg-green-500/10 p-3 text-green-500">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
-            </svg>
-
+        <div className="relative flex justify-around items-center gap-4 py-3 pl-4 pr-8 bg-gray-800 shadow-lg rounded-xl shadow-teal-900/5">
+          <span className="relative z-[2] w-max flex-shrink-0 overflow-hidden rounded-full bg-teal-500/10 p-3 text-teal-500">
+            <CubeTransparentIcon className='w-4 h-4' />
           </span>
+
+          {/* name and stipend */}
           <div className="flex flex-col gap-1">
-            <h6
-              className="block font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-gray-200">
-              Company Name
+            <h6 className="block font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-gray-200">
+              {props.name}
             </h6>
-            <span className="flex">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 mr-2 text-gray-500" >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15 8.25H9m6 3H9m3 6-3-3h1.5a3 3 0 1 0 0-6M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-              </svg>
-              <p className="block font-sans text-sm antialiased font-normal leading-normal text-gray-500">Stipend</p>
+            <span className="inline-flex text-gray-400">
+             <CurrencyRupeeIcon className='w-4 h-4 mr-1' />
+              <p className="block font-sans text-sm antialiased font-normal leading-normal text-gray-500">{props.stipend}</p>
             </span>
           </div>
-          <ChevronRight/>
+          <span className="flex text-teal-600">
+View
+          <ChevronRightIcon className='w-4 h-4' />
+          </span>
+          {/* <span className="relative z-[2] w-max flex-shrink-0 overflow-hidden rounded-full bg-teal-500/10 p-3 text-teal-500">
+          </span> */}
+
         </div>
 
       </div>
     </>
   )
 }
+
+CompanyRecom.defaultProps={
+  stipend:'Stipend',
+  name:'Company Name',
+}
+
 export default CompanyRecom
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,30 @@
 import React from 'react'
 import CompanyRecom from './CompanyRecom'
 import Footer from './Footer'
-import {Clock} from "heroicons"
+import { ClockIcon } from "@heroicons/react/24/outline"
 const Home = () => {
-  // let d = 'Wed, 27 January 2024';
+  let d = 'Wed, 27 January 2024';
   return (
     <>
       <main className='flex flex-col space-y-12'>
-      <section className="flex flex-col">
-        <div className="mb-3">
-          <span className='flex text-gray-400' >
-            <Clock/>
-            Wed, 27 January 2024</span>
-          <h1 className="title-font font-medium text-3xl text-white ">Your Skills</h1>
-        </div>
+        <section className="flex flex-col">
+          <div className="mb-3">
+            <span className='flex text-gray-400' >
+              <ClockIcon className='w-4 h-4' />
+              {d}</span>
+            <h1 className="title-font font-medium text-3xl text-white ">Your Skills</h1>
+          </div>
+          <div className="w-100 h-48 bg-gray-700 rounded-xl"></div>
+        </section>
 
-        <div className="w-100 h-48 bg-gray-700 rounded-xl"></div>
-      </section>
-      
-<section>
+        <section>
 
-<h1 className="title-font font-medium text-3xl text-white ">Company Recoms</h1>
-      <CompanyRecom />
-      <CompanyRecom />
-      <CompanyRecom />
-</section>
-      <Footer />
+          <h1 className="title-font font-medium text-3xl text-white ">Company Recoms</h1>
+          <CompanyRecom />
+          <CompanyRecom />
+          <CompanyRecom />
+        </section>
+        <Footer />
       </main>
     </>
   )

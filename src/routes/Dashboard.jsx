e0d550dee0a1a3ccb1a49a1cc9a09e256ff7c9b8
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2024-01-27 20:02:45 +0530

    behold! dashboard

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,8 +1,36 @@
-export default function Dashboard() {
+import CompanyRecom from "../components/CompanyRecom";
+import Footer from "../components/Footer";
+import { ClockIcon } from "@heroicons/react/24/outline";
+const Home = () => {
+    let d = "Wed, 27 January 2024";
     return (
-        <div className="dashboard">
-            <h1>Dashboard</h1>
-            <p>Protected route</p>
-        </div>
+        <>
+            <main className="flex flex-col space-y-12">
+                <section className="flex flex-col">
+                    <div className="mb-3">
+                        <span className="flex text-gray-400">
+                            <ClockIcon className="w-4 h-4" />
+                            {d}
+                        </span>
+                        <h1 className="title-font font-medium text-3xl text-white ">
+                            Your Skills
+                        </h1>
+                    </div>
+                    <div className="w-100 h-48 bg-gray-700 rounded-xl"></div>
+                </section>
+
+                <section>
+                    <h1 className="title-font font-medium text-3xl text-white ">
+                        Company Recoms
+                    </h1>
+                    <CompanyRecom />
+                    <CompanyRecom />
+                    <CompanyRecom />
+                </section>
+                <Footer />
+            </main>
+        </>
     );
-}
+};
+
+export default Home;
